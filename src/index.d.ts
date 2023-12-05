03b30df46be408b28f3633a3cc0b94d9ede0f0af
Owen Rees <owenrees82@gmail.com>
2023-12-05 11:51:47 +0100

    readme

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,22 +1,22 @@
 export interface Diagram {
-  ply: number
-  fen: string
+    ply: number;
+    fen: string;
 }
 export default class Pgn2Tex {
-  private readonly pgn
-  private diagrams
-  private readonly game
-  private moveStr
-  private readonly texStart
-  private readonly texEnd
-  private readonly header
-  private moves
-  constructor(pgn: string, diagrams: Diagram[])
-  private addThreeDots
-  private sideToMove
-  private commentsAfter
-  private diagram
-  private variations
-  private format
-  toTex(): string
+    private readonly pgn;
+    private diagrams;
+    private readonly game;
+    private moveStr;
+    private readonly texStart;
+    private readonly texEnd;
+    private readonly header;
+    private moves;
+    constructor(pgn: string, diagrams: Diagram[]);
+    private addThreeDots;
+    private sideToMove;
+    private commentsAfter;
+    private diagram;
+    private variations;
+    private format;
+    toTex(): string;
 }
