64c84d787426eb7cfd4ed38c0586c42a2a29d851
Owen <owenrees.dev@gmail.com>
2026-02-25 13:57:23 +0100

    commented code as idea for JS interop

---
+++ bin/main.ml
@@ -0,0 +1,43 @@
+(* open Js_of_ocaml
+open Pgn_logic
+
+(* Converts a JSON string containing {"5": "fen...", "10": "fen..."} into a MoveMap *)
+let parse_diagram_json json_str =
+  let js_json_str = Js.string json_str in
+  (* Call JS native JSON.parse *)
+  let js_obj = Js._JSON##parse js_json_str in
+
+  (* Get all keys from the parsed JS object *)
+  let keys = Js.object_keys js_obj |> Js.to_array |> Array.to_list in
+
+  List.fold_left
+    (fun acc js_key ->
+      let key_str = Js.to_string js_key in
+      (* Dynamically get the value for each key *)
+      let js_value = Js.Unsafe.get js_obj js_key in
+      let value_str = Js.to_string js_value in
+
+      match int_of_string_opt key_str with
+      | Some k -> Pgn2tex.MoveMap.add k value_str acc
+      | None -> acc)
+    Pgn2tex.MoveMap.empty keys
+
+(* The wrapper function that JS will call *)
+let convert_js pgn_js diagram_json_js =
+  let pgn = Js.to_string pgn_js in
+  let json_str = Js.to_string diagram_json_js in
+
+  let diagram_data =
+    if json_str = "" || json_str = "{}" then Pgn2tex.MoveMap.empty
+    else parse_diagram_json json_str
+  in
+
+  let result = Pgn2tex.to_tex pgn diagram_data in
+  Js.string result
+
+(* Exporting the module to the global JavaScript scope *)
+let () =
+  Js.export "Pgn2tex"
+    (object%js
+       method convert pgn diagram_json = convert_js pgn diagram_json
+    end) *)
